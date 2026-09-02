bef1b1eaa396cd8018d76fc786e36af0d9215d9c
jean <kjeanchrist98@gmail.com>
2026-09-02 14:36:53 +0000

    feat: utilisation de mutableStateOf pour rendre l'état observable

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -58,7 +59,6 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-
         EditNumberField(
             modifier = Modifier
                 .padding(bottom = 32.dp)
@@ -72,13 +72,13 @@ fun TipTimeLayout() {
     }
 }
 
-// Fonction ajoutée selon la diapositive
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    val amountInput = "0"
+    var amountInput = mutableStateOf("0")
+
     TextField(
-        value = amountInput,
-        onValueChange = {},
+        value = amountInput.value,
+        onValueChange = { amountInput.value = it },
         modifier = modifier
     )
 }
